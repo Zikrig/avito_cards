67d65f3af250a0eda04910cdc8267878b0acc427
Fedor Grizik <fedorgrif@gmail.com>
2026-02-25 16:04:32 +0300

    аа

--- main.py
+++ main.py
@@ -55,6 +55,12 @@ class CardStates(StatesGroup):
     waiting_for_price = State()
 
 
+class ExampleStates(StatesGroup):
+    waiting_for_photos = State()
+    waiting_for_features = State()
+    waiting_for_description = State()
+    waiting_for_price = State()
+
 class ConfigStates(StatesGroup):
     waiting_for_value = State()
 
